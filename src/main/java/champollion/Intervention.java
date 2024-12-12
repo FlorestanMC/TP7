0809603120a3b079af750a2721f2d8a912b6dcc9
Florestan MC <florestan.melle@gmail.com>
2024-12-12 10:26:19 +0100

    partiel

---
+++ src/main/java/champollion/Intervention.java
@@ -0,0 +1,12 @@
+package champollion;
+
+import java.util.Date;
+
+public class Intervention {
+    private Date debut;
+    private int duree;
+    private boolean annulee = false;
+    private int HeureDebut;
+
+
+}
